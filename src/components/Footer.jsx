6006789949db79d16eacd7fa871852197dddd069
Yanisse MOUNIB <ymounib@amiltone.com>
2023-03-14 13:36:07 +0100

    modification footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,24 @@
 const footerBar = () =>{
     return <footer>
         <img src="https://www.dynamicmarketing.eu/wp-content/uploads/2018/06/ecommerce.logo_.png" alt="Logo e-commerce" className="logo-e-commerce" />
+                <nav>
+                    <ul>
+                        <li><a href="">A Propos</a></li>
+                        <li><a href="">Policies</a></li> 
+                    </ul>
+                    <ul>
+                        <li><a href="">Chartes de confidentialités</a></li>
+                        <li><a href="">Contact</a></li>
+                    </ul>
+                </nav>
+                
                 <div>
-                    {/* <a target="_blank" href="https://twitter.com/" className="logo-reseaux">
-                        <img src="https://cdn.icon-icons.com/icons2/1143/PNG/512/twitterlogooutline_80724.png" alt="Logo Twitter" />
+                <a target="_blank" href="https://twitter.com/">
+                        <img src="https://cdn.icon-icons.com/icons2/1143/PNG/512/twitterlogooutline_80724.png" alt="Logo Twitter" className="logo-reseaux" />
                     </a>
-                    <a target="_blank" href="https://www.instagram.com/" className="logo-reseaux">
-                        <img src="https://cdn-icons-png.flaticon.com/512/87/87390.png" alt="logo d'Instagram" />
-                    </a>  */}
+                    <a target="_blank" href="https://www.instagram.com/">
+                        <img src="https://cdn-icons-png.flaticon.com/512/87/87390.png" alt="logo d'Instagram" className="logo-reseaux" />
+                    </a> 
                 </div>
     </footer>;
 }
