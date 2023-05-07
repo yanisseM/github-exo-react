192de3acacc83534dd9deaaa499832ee18fd8b45
vteerencee <125984850+vteerencee@users.noreply.github.com>
2023-05-07 11:03:15 +0200

    20230507terence

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,21 +1,43 @@
 import React from "react";
 import "../index.css";
 
-const Product = () => {
+const Product = ({title, price, imageSrc}) => {
   return (
     <div className="card product-card">
       <img
         className="card-img-top product-card-image"
-        src="https://picsum.photos/id/237/200/300"
+        src={imageSrc}
         alt="Product"
       />
       <div className="card-body product-card-details">
-        <h2 className="card-title product-card-title">Nom du produit</h2>
-        <span className="product-price">29.99 €</span>
+        <h2 className="card-title product-card-title">{title}</h2>
+        <span className="product-price">{price}</span>
         <button className="btn btn-primary add-to-cart-button">Ajouter au panier</button>
       </div>
     </div>
   );
 };
 
-export default Product;
+const Products = () => {
+  return (
+    <div className="products">
+      <Product
+        title="Crampons Nike"
+        price="29.99 €"
+        imageSrc="C:\Users\teren\Documents\ESTIAM\github-exo-react\src\image\ol.jpg"
+      />
+      <Product
+        title="Ballon de foot Adidas"
+        price="14.99 €"
+        imageSrc="C:\Users\teren\Documents\ESTIAM\github-exo-react\src\image\ballon.jpg"
+      />
+      <Product
+        title="Maillot de foot PSG"
+        price="79.99 €"
+        imageSrc="C:\Users\teren\Documents\ESTIAM\github-exo-react\src\image\maillot.jpg"
+      />
+    </div>
+  );
+};
+
+export default Products;
